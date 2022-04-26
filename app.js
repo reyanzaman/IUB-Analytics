906d8e90a51643b368048683f29799e7ef21ff90
Reyan Zaman <reyan.luna@gmail.com>
2022-04-26 14:44:44 +0600

    Automated upload fixes

--- app.js
+++ app.js
@@ -108,8 +108,22 @@ function TruncateTable(query) {
   db.query(query, (error, response) => {
     console.log("Truncate Query For Tally Sheet");
     console.log(error || response);
-    console.log('All the Queries has been executed successfully');
+    console.log('Most of the Queries has been executed successfully');
   });
+}
+
+function ExtraTable(query){
+  db.query(query, (error, response) => {
+    console.log("Extra Table Query");
+    console.log(error || response);
+  })
+}
+
+function AlterTable(query){
+  db.query(query, (error, response) => {
+    console.log("Alter Table Query");
+    console.log(error || response);
+  })
 }
 
 //-----------------------------------------------
@@ -134,24 +148,60 @@ function UploadCsvDataToMySQL(filePath, names) {
           console.log(names.name);
           console.log('Query Execution Started.');
           var tName = dbConvert(names.name);
+          //Order 1 Insert Table Into Database
           let query1 = 'INSERT INTO tallysheet (SCHOOL_TITLE, COFFER_COURSE_ID, COFFERED_WITH, SECTION, CREDIT_HOUR, CAPACITY, ENROLLED, ROOM_ID, ROOM_CAPACITY, BLOCKED, COURSE_NAME, FACULTY_FULL_NAME, STRAT_TIME, END_TIME, ST_MW) VALUES ?';
           InsertQuery(query1, csvData);
-          let query2 = 'CREATE TABLE ' + tName + '_course AS SELECT DISTINCT COFFER_COURSE_ID, COURSE_NAME, SCHOOL_TITLE FROM tallysheet ORDER BY SCHOOL_TITLE;';
+          //Order 2 Create Table Sem_Class
+          let query2 = 'CREATE TABLE ' + tName + '_class ENGINE=InnoDB AS SELECT COFFER_COURSE_ID, SECTION, CREDIT_HOUR, ENROLLED, CAPACITY, ROOM_ID, FACULTY_FULL_NAME, STRAT_TIME AS START_TIME, END_TIME, ST_MW FROM tallysheet ORDER BY COFFER_COURSE_ID, SECTION;';
           setTimeout(function () {
-            MigrateQueryCourse(query2);
-          }, 1500);
-          let query3 = 'CREATE TABLE ' + tName + '_room AS SELECT DISTINCT ROOM_ID, ROOM_CAPACITY FROM tallysheet ORDER BY ROOM_ID;';
-          setTimeout(function () {
-            MigrateQueryRoom(query3);
+            MigrateQueryClass(query2);
           }, 1600);
-          let query4 = 'CREATE TABLE ' + tName + '_class AS SELECT COFFER_COURSE_ID, SECTION, CREDIT_HOUR, ENROLLED, CAPACITY, ROOM_ID, FACULTY_FULL_NAME, STRAT_TIME AS START_TIME, END_TIME, ST_MW FROM tallysheet ORDER BY COFFER_COURSE_ID, SECTION;';
+          //Order 3 Create Table Sem_course
+          let query3 = 'CREATE TABLE ' + tName + '_course ENGINE=InnoDB AS SELECT DISTINCT COFFER_COURSE_ID, COURSE_NAME, SCHOOL_TITLE FROM tallysheet ORDER BY SCHOOL_TITLE;';
           setTimeout(function () {
-            MigrateQueryClass(query4);
-          }, 1700);
-          let query5 = 'DELETE FROM tallysheet';
+            MigrateQueryCourse(query3);
+          }, 2000);
+          //Order 4 Create Table Sem_room
+          let query4 = 'CREATE TABLE ' + tName + '_room ENGINE=InnoDB AS SELECT DISTINCT ROOM_ID, ROOM_CAPACITY FROM tallysheet ORDER BY ROOM_ID;';
           setTimeout(function () {
-            TruncateTable(query5);
-          }, 4000);
+            MigrateQueryRoom(query4);
+          }, 2400);
+          //Order 5 Create Table Sem_extra
+          let query5 = 'CREATE TABLE ' + tName + '_extra ENGINE=InnoDB AS SELECT COFFER_COURSE_ID, SECTION, COFFERED_WITH, BLOCKED FROM tallysheet ORDER BY COFFER_COURSE_ID, SECTION';
+          setTimeout(function () {
+            ExtraTable(query5);
+          }, 2800);
+          //Order 6
+          let query6 = 'DELETE FROM tallysheet';
+          setTimeout(function () {
+            TruncateTable(query6);
+          }, 5000);
+
+          //Alter Table to add Primary Keys
+          //Primary Key and Foreign keys code has been commented out to prevent dummy data from causing errors.
+          //Dummy data containts random values which does not follow the primary and foreign key constraint.
+          //So, for simplicity's sake, the code has been commented out.
+
+        //   //Order 7
+        //   let query7 = 'ALTER TABLE ' + tName + '_class ADD CONSTRAINT ' + tName + '_class_PK PRIMARY KEY (COFFER_COURSE_ID, SECTION), ADD CONSTRAINT ' + tName + '_class_FK1 FOREIGN KEY(COFFER_COURSE_ID) REFERENCES ' + tName + '_course(COFFER_COURSE_ID), ADD CONSTRAINT ' + tName + '_class_FK2 FOREIGN KEY(ROOM_ID) REFERENCES ' + tName + '_room(ROOM_ID);';
+        //   setTimeout(function () {
+        //     AlterTable(query7);
+        //   }, 5200);
+        //   //Order 8
+        //   let query8 = 'ALTER TABLE ' + tName + '_course ADD CONSTRAINT ' + tName + '_course_PK PRIMARY KEY (COFFER_COURSE_ID);';
+        //   setTimeout(function () {
+        //     AlterTable(query8);
+        //   }, 5500);
+        //   //Order 9
+        //   let query9 = 'ALTER TABLE ' + tName + '_room ADD CONSTRAINT ' + tName + '_room_PK PRIMARY KEY (ROOM_ID);';
+        //   setTimeout(function () {
+        //     AlterTable(query9);
+        //   }, 5600);
+        //   //Order 10
+        //   let query10 = 'ALTER TABLE ' + tName + '_extra ADD CONSTRAINT ' + tName + '_extra_PK PRIMARY KEY (COFFER_COURSE_ID, SECTION), ADD CONSTRAINT ' + tName + '_extra_FK1 FOREIGN KEY(COFFER_COURSE_ID) REFERENCES ' + tName + '_class(COFFER_COURSE_ID), ADD CONSTRAINT ' + tName + '_extra_FK2 FOREIGN KEY(SECTION) REFERENCES ' + tName + '_class(SECTION);';
+        //   setTimeout(function () {
+        //     AlterTable(query10);
+        //   }, 5700);
         }
       });
       // delete temporary file after saving to MySQL database
